f842d8cc3e5abb6941cc0108b7c1a4cc5e34dcdc
bss3@cin.ufpe.br <bss3@cin.ufpe.br@8c26b5b9-64aa-521a-b4d2-9eb79cc5554e>
2012-07-04 19:18:35 +0000

    Inserção das Classes de Rede

---
+++ trunk/src/rede/OperacoesBinarias.java
@@ -0,0 +1,112 @@
+package rede;
+
+public class OperacoesBinarias {
+
+	//ok!
+	public static  void intParaByte(byte[] retorno,int inteiro,int posicao,boolean isHalf, boolean isByte){ //Convertendo inteiro para array de bytes
+
+		if(posicao>=0){
+			if(isByte){
+				if(posicao<retorno.length){
+					retorno[posicao] = (byte) (inteiro  & 0xFF);
+				}
+			}else if(isHalf){
+				if(posicao<(retorno.length-2)){
+					retorno[posicao] = (byte) (inteiro  & 0xFF);
+					retorno[posicao+1] = (byte) ((inteiro>>8)  & 0xFF);
+				}
+			}else{
+				if(posicao<(retorno.length-4)){
+					retorno[posicao] = (byte) (inteiro  & 0xFF);
+					retorno[posicao+1] = (byte) ((inteiro>>8)  & 0xFF);
+					retorno[posicao+2] = (byte) ((inteiro>>16)  & 0xFF);
+					retorno[posicao+3] = (byte) ((inteiro>>24)  & 0xFF);
+				}
+			}
+		}		
+	}
+
+	//ok!
+	public static int bytesParaInt (byte[] numero,int posicao,boolean isHalf, boolean isByte){
+		int retorno = 0; //retorno default
+		if(posicao >=0){
+			if(isByte){
+				if(posicao<(numero.length))
+				return  (numero[posicao] & 0xFF);
+			}else if(isHalf){
+				if(posicao<(numero.length-2))
+				return  (numero[posicao+1] & 0xFF)<<8 | (numero[posicao] & 0xFF);
+			}else{
+				if(posicao<(numero.length-4))
+				return  (numero[posicao+3] & 0xFF) << 24 | (numero[posicao+2] & 0xFF) << 16 | (numero[posicao+1] & 0xFF)<<8 | (numero[posicao] & 0xFF); //Fazendo o processo inverso de intParabyte
+			}
+
+		}
+		return retorno;
+	}
+
+	//ok!
+	public static void inserirCabecalho(byte[]dados,int numeroSequencia, int numeroReconhecimento,boolean ACK, boolean RST,boolean SYN, boolean FIN,int comprimentoDados,int janelaRecepcao ){
+		intParaByte(dados,numeroSequencia,0,false,false);
+		intParaByte(dados, numeroReconhecimento, 4,false,false);
+		intParaByte(dados, comprimentoDados, 8, true, false);
+		intParaByte(dados, janelaRecepcao, 10, true, false);
+		intParaByte(dados, ACK?1:0, 12, false, true);
+		intParaByte(dados, RST?1:0, 13, false, true);
+		intParaByte(dados, SYN?1:0, 14, false, true);
+		intParaByte(dados, FIN?1:0, 15, false, true);
+	}
+
+	//ok!
+	public static int extrairNumeroSequencia(byte[] data){
+		return bytesParaInt(data, 0, false, false);
+	}
+
+	//ok!
+	public static int extrairNumeroReconhecimento(byte[] data){
+		return bytesParaInt(data, 4, false, false);
+	}
+
+	//ok!
+	public static boolean extrairACK(byte[] data){
+		if(bytesParaInt(data, 12, false, true)==1)
+			return true;
+		else
+			return false;
+	}
+
+	//ok!
+	public static boolean extrairRST(byte[] data){
+		if(bytesParaInt(data, 13, false, true)==1)
+			return true;
+		else
+			return false;
+	}
+
+	//ok!
+	public static boolean extrairSYN(byte[] data){
+		if(bytesParaInt(data, 14, false, true)==1)
+			return true;
+		else
+			return false;
+	}
+
+	//ok!
+	public static boolean extrairFIN(byte[] data){
+		if(bytesParaInt(data, 15, false, true)==1)
+			return true;
+		else
+			return false;
+	}
+
+	//ok!
+	public static int extrairComprimentoDados(byte[] data){
+		return bytesParaInt(data, 8, true, false);
+	}
+
+	//ok!
+	public static int extrairJanelaRecepcao(byte[] data){
+		return bytesParaInt(data,10, true, false);
+	}
+
+}
---
+++ trunk/src/rede/Pacote.java
@@ -0,0 +1,66 @@
+package rede;
+import java.io.Serializable;
+import java.net.DatagramPacket;
+
+
+
+@SuppressWarnings("serial")
+public class Pacote implements Serializable {
+	
+	DatagramPacket pkt;
+	private boolean is_send;
+	public static final int head_payload = 16;
+	public static int default_size = 1040;
+	public static int util_load = default_size-head_payload;
+	long send_time;
+	
+	
+	public Pacote(DatagramPacket pkt, long send_time){
+		this.pkt = pkt;
+		this.is_send = false;
+		this.send_time = send_time;
+	}
+	
+	public Pacote(DatagramPacket pkt){
+		this.pkt = pkt;
+		this.is_send = false;
+		this.send_time = -1;
+	}
+	
+	public boolean isEnviado() {
+		return is_send;
+	}
+
+	public void setEnviado(boolean enviado) {
+		this.is_send = enviado;
+	}
+
+
+	public int getNumeroSequencia() {
+		return OperacoesBinarias.extrairNumeroSequencia(this.pkt.getData());
+	}
+
+	public static int getTamanhoPadrao() {
+		return default_size;
+	}
+
+	public byte[] getData() {
+		return this.pkt.getData();
+	}
+
+	public void setSend_time(long new_send_time){
+		this.send_time = new_send_time;
+	}
+	
+	public Pacote setSend_time_and_getme(long new_send_time){
+		this.send_time = new_send_time;
+		return this;
+	}
+	
+	@Override
+	public String  toString(){
+		int seqNum = OperacoesBinarias.extrairNumeroSequencia(this.pkt.getData());
+		int lenthg = OperacoesBinarias.extrairComprimentoDados(this.pkt.getData());
+		return "["+seqNum+"]\nState: "+this.is_send;
+	}
+}
