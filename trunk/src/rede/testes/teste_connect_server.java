43d65fa69adbf585456abf86ab3b978f510a6b1f
bss3@cin.ufpe.br <bss3@cin.ufpe.br@8c26b5b9-64aa-521a-b4d2-9eb79cc5554e>
2012-07-06 00:35:44 +0000

    ServerSocket.accept funcionando

--- trunk/src/rede/testes/teste_connect_server.java
+++ trunk/src/rede/testes/teste_connect_server.java
@@ -8,5 +8,6 @@ public class teste_connect_server {
 	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(3000);
 		serverSocket.accept();
+		System.out.println("Conectado");
 	}
 }
