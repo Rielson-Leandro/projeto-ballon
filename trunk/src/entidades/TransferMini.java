85a934a26d3ef3a085bc198da4c32277589f90e9
phfn@cin.ufpe.br <phfn@cin.ufpe.br@8c26b5b9-64aa-521a-b4d2-9eb79cc5554e>
2012-07-09 01:50:16 +0000

    Atualizacao para minitcp 3

--- trunk/src/entidades/TransferMini.java
+++ trunk/src/entidades/TransferMini.java
@@ -18,93 +18,99 @@ public class TransferMini extends Thread{
 	private ReceberArquivo reciever;
 	private String caminhoArquivo;
 	private int portaTransferencia;
-	
+
 	//campos sender
 	private long seek;
-	
+
 	//campos reciever
 	private InetAddress endereco;
 	private long tamanhoArquivo;
 	private boolean isAppend;
-	
+
 	public TransferMini(){
 		this.isClient = false;
 	}
-	
+
 	public TransferMini(Arquivo arq) {
 		this.arquivo = arq;
 		this.isClient = true;
 	}
-	
+
 	public void setSender(String caminho_arquivo,int porta_envio,long seek){
 		this.isSender = true;
 		this.isReciever = false;
-		
+
 		this.caminhoArquivo = caminho_arquivo;
 		this.portaTransferencia = porta_envio;
 		this.seek = seek;
 	}
-	
+
 	public void setReciever(String caminho_arquivo, int porta_servidor_arquivo, InetAddress endereco_servidor_sarquivo,long tamanhoArquivo, boolean continuacao_arquivo){
 		this.isSender = false;
 		this.isReciever = true;
-		
+
 		this.caminhoArquivo = caminho_arquivo;
 		this.portaTransferencia = porta_servidor_arquivo;
 		this.endereco = endereco_servidor_sarquivo;
 		this.tamanhoArquivo = tamanhoArquivo;
 		this.isAppend = continuacao_arquivo;
 	}
-	
+
 	public void run(){
-		
+
 		if(this.isSender){
-			
+
 			try {
 				this.sender = new EnviarArquivo(this.caminhoArquivo, this.portaTransferencia, this.seek);
-				
+
 				while(this.sender.getPorcentagem() < 100){
-					System.out.println("Enviados: " + this.sender.getPorcentagem() + "%");
+					System.out.println("Enviados: " + (int) this.sender.getPorcentagem() + "%");
 					try {
 						this.sleep(1000);
 					} catch (InterruptedException e) {
 						System.out.println("FALHA ao enviar a thread de envio para sleep.");
 					}
 				}
-				
-				this.arquivo.setSyncing(false);
-				this.arquivo.setSyncStatus(true);
-				
+
+				if(this.isClient){
+					this.arquivo.setSyncing(false);
+					this.arquivo.setSyncStatus(true);
+				}
+
 			} catch (ArquivoNaoEncontradoException e) {
 				System.out.println("FALHA ao enviar o arquivo " + this.caminhoArquivo + " ... Arquivo nao encontrado.");
 			} catch (ErroConexaoException e) {
 				System.out.println("FALHA de conexao ao enviar " + this.caminhoArquivo);
 			}	
 		}
-		
+
 		if(this.isReciever){
-			
+
 			try {
 				this.reciever = new ReceberArquivo(this.caminhoArquivo, this.portaTransferencia, this.endereco, this.tamanhoArquivo, this.isAppend);
-				
-				while(this.sender.getPorcentagem() < 100){
-					System.out.println("Recebidos: " + this.sender.getPorcentagem() + "%");
+
+				while(this.reciever.getPorcentagem() < 100){
+					System.out.println("Recebidos: " + (int) this.reciever.getPorcentagem() + "%");
 					try {
 						this.sleep(1000);
 					} catch (InterruptedException e) {
 						System.out.println("FALHA ao enviar a thread de envio para sleep.");
 					}
 				}
-				
-				this.arquivo.setSyncing(false);
-				this.arquivo.setSyncStatus(true);
-				
+
+				if(isClient){
+					this.arquivo.setSyncing(false);
+					this.arquivo.setSyncStatus(true);
+				}
+
 			} catch (IOException e1) {
 				System.out.println("FALHA ao receber arquivo em " + this.caminhoArquivo);
 			}
-			
+
 		}
-		
+
+		System.out.println("Finalizando transferidor.");
+
 	}
-	
+
 }
